c22d6047749f5acc9a1603eb74e10fd667381272
nabilshukri <nabilshukri2001@gmail.com>
2024-05-23 19:36:10 +0800

    First Commit

---
+++ src/main/java/com/example/secureAssignment/SecureAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.secureAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/secureAssignment/SecureAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secureAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
