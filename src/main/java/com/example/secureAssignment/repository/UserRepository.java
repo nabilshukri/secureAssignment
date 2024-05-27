e9976fea8c3e20d5590b02d4d95112266fe3357a
nabilshukri <nabilshukri2001@gmail.com>
2024-05-27 20:11:46 +0800

    Create UserRepository and RoleRepository

---
+++ src/main/java/com/example/secureAssignment/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.secureAssignment.repository;
+
+import com.example.secureAssignment.models.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<ApplicationUser, Integer> {
+
+    Optional<ApplicationUser> findByUsername(String username);
+}
